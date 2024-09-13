17a3899941b7a5ffd0f1c5fa5f6b4709ec75a15e
Patten34 <yanuntorn.scada@gmail.com>
2024-09-13 16:04:29 +0700

    Add project files.

---
+++ Netflix/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Netflix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Start a timer when the MainWindow is loaded
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize a dispatcher timer
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(3); // Set the timer for 5 seconds
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _timer.Stop(); // Stop the timer to avoid repeating
+
+            // Navigate to the next page
+            Profile profilePage = new Profile();
+            this.Content = profilePage;
+        }
+    }
+}
---
+++ Netflix/Profile.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Netflix
+{
+    /// <summary>
+    /// Interaction logic for Profile.xaml
+    /// </summary>
+    public partial class Profile : Page
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Profile 1 click
+            MessageBox.Show("Edit (Ememalo) selected!");
+        }
+        private void Profile1_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Profile 1 click
+            MessageBox.Show("Profile 1 (Ememalo) selected!");
+        }
+
+        private void Profile2_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Profile 2 click
+            MessageBox.Show("Profile 2 (Onyeka) selected!");
+        }
+
+        private void Profile3_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Profile 3 click
+            MessageBox.Show("Profile 3 (Thelma) selected!");
+        }
+
+        private void Profile4_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Profile 4 click
+            MessageBox.Show("Profile 4 (Kids) selected!");
+        }
+
+        private void AddProfile_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for Add Profile button
+            MessageBox.Show("Add Profile button clicked!");
+        }
+
+    }
+}
